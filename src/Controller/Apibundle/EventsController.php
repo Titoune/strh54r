749739e88913fdd9ecd3582065475941cd8a27af
Titoune <cleobule71100@gmail.com>
2017-11-12 00:47:30 +0100

    Avancement

--- src/Controller/Apibundle/EventsController.php
+++ src/Controller/Apibundle/EventsController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Controller\Apibundle;
+use App\Utility\Tools;
 
 
 /**
@@ -12,104 +14,11 @@ namespace App\Controller\Apibundle;
 class EventsController extends InitController
 {
 
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|void
-     */
-    public function index()
+    public function getEvents()
     {
-        $this->paginate = [
-            'contain' => ['Users']
-        ];
-        $events = $this->paginate($this->Events);
+        $payloads = Tools::decodeJwt($this->request->getHeaderLine('Authorization'));
+        $events = $this->Events->find()->order(['Events.created' => 'asc']);
+        $this->apiResponse['data']['events'] = $events;
 
-        $this->set(compact('events'));
-        $this->set('_serialize', ['events']);
-    }
-
-    /**
-     * View method
-     *
-     * @param string|null $id Event id.
-     * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $event = $this->Events->get($id, [
-            'contain' => ['Users', 'EventComments', 'EventParticipations']
-        ]);
-
-        $this->set('event', $event);
-        $this->set('_serialize', ['event']);
-    }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $event = $this->Events->newEntity();
-        if ($this->request->is('post')) {
-            $event = $this->Events->patchEntity($event, $this->request->getData());
-            if ($this->Events->save($event)) {
-                $this->Flash->success(__('The event has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The event could not be saved. Please, try again.'));
-        }
-        $users = $this->Events->Users->find('list', ['limit' => 200]);
-        $this->set(compact('event', 'users'));
-        $this->set('_serialize', ['event']);
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Event id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $event = $this->Events->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $event = $this->Events->patchEntity($event, $this->request->getData());
-            if ($this->Events->save($event)) {
-                $this->Flash->success(__('The event has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The event could not be saved. Please, try again.'));
-        }
-        $users = $this->Events->Users->find('list', ['limit' => 200]);
-        $this->set(compact('event', 'users'));
-        $this->set('_serialize', ['event']);
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Event id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $event = $this->Events->get($id);
-        if ($this->Events->delete($event)) {
-            $this->Flash->success(__('The event has been deleted.'));
-        } else {
-            $this->Flash->error(__('The event could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
     }
 }
